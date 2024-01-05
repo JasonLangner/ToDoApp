3f9c5c9bc05b64af7c06797869b106899f493104
Jason Langner <jason.langner@neuwaerts.de>
2024-01-05 14:55:01 +0100

    Create filter for incomplete and complete tasks

--- index.js
+++ index.js
@@ -1,23 +1,69 @@
 let tasks = document.querySelector('.tasks')
+let buttonList = document.querySelectorAll('.button');
+let incomplete = document.querySelector('.incomplete');
+let complete = document.querySelector('.complete');
+
+// MutationObserver
 const config = { attributes: true, childList: true, subtree: true };
 
 let progress = document.querySelector('.sticky-progress-fill');
 
-
 const callback = (mutationList, observer) => {
-    let taskAll = document.querySelectorAll(".task");
-    let taskComplete = document.querySelectorAll(".task.done");
+  let taskAll = document.querySelectorAll(".task");
 
-  var progressPercent = 100/taskAll.length*taskComplete.length;
-   progress.style.width = `${progressPercent}%`;
+  if (taskAll.length === 0) {
+    return (progress.style.width = `0%`)
   }
 
+  let taskComplete = document.querySelectorAll(".task.done");
+  let taskIncomplete = tasks.querySelectorAll("li:not(.done)");
+
+  var progressPercent = 100 / taskAll.length * taskComplete.length;
+  progress.style.width = `${progressPercent}%`;
+
+  if (complete.classList.contains('active')) {
+    for (let task of taskIncomplete) {
+      task.style.display = ('none');
+    } 
+  } else {
+    for (let task of taskIncomplete) {
+      task.style.display = ('flex');
+    } 
+  }
+
+  if (incomplete.classList.contains('active')) {
+    for (let task of taskComplete) {
+      task.style.display = ('none');
+    }
+  } else {
+    for (let task of taskComplete) {
+      task.style.display = ('flex');
+    } 
+  }
+}
+  
 
 const observer = new MutationObserver(callback);
 
 observer.observe(tasks, config);
 
+for (let button of buttonList) {
+  button.addEventListener('click', () => {
 
+    for (let button2 of buttonList) {
+      button2.classList.remove('active');
+    }
+    if (!button.classList.contains('reset'))
+      button.classList.add('active');
+  }
+  )
+}
+
+
+
+
+
+// create Task
 const createTask = (prompt) => {
 
   const html = `
@@ -41,7 +87,7 @@ const createTask = (prompt) => {
 
   function doneTask() {
     node.classList.toggle('done');
-    
+
     if (node.classList.contains('done')) {
       done.querySelector('.icon').src = '/icons/Icons=Undo.svg';
     } else {
@@ -74,10 +120,10 @@ stickyadd.addEventListener('click', () => {
     }
     createTask(task);
   }
-  catch (error){
+  catch (error) {
     console.warn(error);
     alert("You didn't wrote a task in your textarea")
-   }
+  }
 
 
 })
